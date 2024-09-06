a4ada61effab7a129e1582f1a1a2c90b868e77d0
mekalablogo <madhan@blogo.ai>
2024-09-06 13:51:12 +0530

    Add files via upload

---
+++ Database_conn.py
@@ -0,0 +1,3 @@
+CONNECTION_STRING = "mongodb://blogomongodb1:Upr1bTQYpyhMqN9W9xLvb2U5ZGKzdgt7EqgN9aqmrUnBQgmccmRYZPYVgev7Sq68Oy9QOP4eeWANACDbhJXghw==@blogomongodb1.mongo.cosmos.azure.com:10255/?ssl=true&retrywrites=false&replicaSet=globaldb&maxIdleTimeMS=120000&appName=@blogomongodb1@"
+DATABASE_NAME = "Blogodb1"
+COLLECTION_NAME = "Blogocl1"
