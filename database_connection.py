3fa0ff67ba954859079879be616057b17263df26
mekalablogo <madhan@blogo.ai>
2024-09-06 13:45:28 +0530

    Update database_connection.py

--- database_connection.py
+++ database_connection.py
@@ -1,19 +1,1 @@
-from Database_conn import COLLECTION_NAME,CONNECTION_STRING,DATABASE_NAME
-from pymongo import MongoClient
-from pymongo.errors import ConnectionFailure
 
-def database_connection():
-
-    try:
-        client = MongoClient(CONNECTION_STRING)
-        db = client[DATABASE_NAME]
-        collection = db[COLLECTION_NAME]
-        return collection
-        print("Connected to Cosmos MongoDB!")
-        
-    except ConnectionFailure as e:
-        print(f"Connection failed: {e}")
-
-
-if __name__ == "__main__":
-    print()
