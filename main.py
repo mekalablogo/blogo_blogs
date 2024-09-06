2f66de8e5153502c65e479d9d4d33d3881ece124
mekalablogo <madhan@blogo.ai>
2024-09-06 15:51:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
-from typing import Optional
-
 from fastapi import FastAPI
+from Blog_Post import Blog_create
+from Get_detail import Blog_detail
+from Slug_based import Slug_detail
 
 app = FastAPI()
 
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+# Include the other applications
+app.mount("/Create a blog", Blog_create)
+app.mount("GetDetail", Blog_detail)
+app.mount("/Blogs/{slug}", Slug_detail)
